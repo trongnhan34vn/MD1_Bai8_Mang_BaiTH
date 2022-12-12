0f7c681051db9247e7c6c4df17a3090ce84be218
Mac <trongnhan34vn@gmail.com>
2022-12-12 22:14:16 +0700

    Bai 8 Mang

--- Bai TH/Bai 2 Dao nguoc phan tu cua mang/MyJS.js
+++ Bai TH/Bai 2 Dao nguoc phan tu cua mang/MyJS.js
@@ -1,4 +1,8 @@
+// Đảo ngược các phần tử trong thẻ
+
+// Input: mảng có 6 phần tử
 let x = [-3, 5, 1, 3, 2, 10];
+// Process:
 let first = 0;
 //biến first duyệt từ phần tử đầu tiên trong mảng
 let last = x.length - 1;
@@ -14,7 +18,7 @@ while (first < last) {
     first ++;
     last --;
 }
-
+// Output: mảng mới đã được đảo ngược các phần tử
 document.write(x);
 
 // document.write(x.reverse())
--- Bai TH/Bai 3 Tim gia tri trong mang/MyJS.js
+++ Bai TH/Bai 3 Tim gia tri trong mang/MyJS.js
@@ -1,7 +1,15 @@
+// Tìm xem giá trị input có trong mảng ko?
+// Input: giá trị prompt input và 1 mảng 6 phần tử
 let value = prompt('Enter the number');
 let numbers = [-3, 5, 1, 3, 2, 10];
+
+// Process: Sử dụng vòng lặp kiểm tra từng phần tử của mảng. từ phần tử đầu tiên i=0
 for(let i = 0; i < numbers.length; i++) {
+    // nếu giá trị nhập vào = giá trị nào đó trong mảng
     if(value == numbers[i]) {
+        // In ra giá trị input trùng với giá trị có trong mảng
         alert('Value' + numbers[i] + 'found at' + i)
     }
 }
+
+//Output: in ra giá trị input trùng với giá trị có trong mảng
